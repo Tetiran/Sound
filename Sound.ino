b1bedf9c5f9cfd02ea212fcd3167caa21f82e69f
Justin Duhamel <jduhamel@seas.upenn.edu>
2022-02-22 01:38:13 -0500

    Init

---
+++ Sound.ino
@@ -0,0 +1,277 @@
+#include <iq_module_communication.hpp>
+
+IqSerial SoundMSerial(Serial1);
+IqSerial MagnetMSerial(Serial2);
+
+MultiTurnAngleControlClient SoundMAngle(0);
+
+MultiTurnAngleControlClient MagnetMAngle(0);
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define REST      0
+
+
+// change this to make the song slower or faster
+int tempo = 200;
+
+// change this to whichever pin you want to use
+int buzzer = 11;
+bool state = false;
+
+float scale = .136;
+
+// notes of the moledy followed by the duration.
+// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
+// !!negative numbers are used to represent dotted notes,
+// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
+int melody[] = {
+
+  // Super Mario Bros theme
+  // Score available at https://musescore.com/user/2123/scores/2145
+  // Theme by Koji Kondo
+  
+  NOTE_E5,8, NOTE_E5,8, REST,8, NOTE_E5,8, REST,8, NOTE_C5,8, NOTE_E5,8, //1
+  NOTE_G5,4, REST,4, NOTE_G4,8, REST,4, 
+  NOTE_C5,-4, NOTE_G4,8, REST,4, NOTE_E4,-4, // 3
+  NOTE_A4,4, NOTE_B4,4, NOTE_AS4,8, NOTE_A4,4,
+  NOTE_G4,-8, NOTE_E5,-8, NOTE_G5,-8, NOTE_A5,4, NOTE_F5,8, NOTE_G5,8,
+  REST,8, NOTE_E5,4,NOTE_C5,8, NOTE_D5,8, NOTE_B4,-4,
+  NOTE_C5,-4, NOTE_G4,8, REST,4, NOTE_E4,-4, // repeats from 3
+  NOTE_A4,4, NOTE_B4,4, NOTE_AS4,8, NOTE_A4,4,
+  NOTE_G4,-8, NOTE_E5,-8, NOTE_G5,-8, NOTE_A5,4, NOTE_F5,8, NOTE_G5,8,
+  REST,8, NOTE_E5,4,NOTE_C5,8, NOTE_D5,8, NOTE_B4,-4,
+
+  
+  REST,4, NOTE_G5,8, NOTE_FS5,8, NOTE_F5,8, NOTE_DS5,4, NOTE_E5,8,//7
+  REST,8, NOTE_GS4,8, NOTE_A4,8, NOTE_C4,8, REST,8, NOTE_A4,8, NOTE_C5,8, NOTE_D5,8,
+  REST,4, NOTE_DS5,4, REST,8, NOTE_D5,-4,
+  NOTE_C5,2, REST,2,
+
+  REST,4, NOTE_G5,8, NOTE_FS5,8, NOTE_F5,8, NOTE_DS5,4, NOTE_E5,8,//repeats from 7
+  REST,8, NOTE_GS4,8, NOTE_A4,8, NOTE_C4,8, REST,8, NOTE_A4,8, NOTE_C5,8, NOTE_D5,8,
+  REST,4, NOTE_DS5,4, REST,8, NOTE_D5,-4,
+  NOTE_C5,2, REST,2,
+
+  NOTE_C5,8, NOTE_C5,4, NOTE_C5,8, REST,8, NOTE_C5,8, NOTE_D5,4,//11
+  NOTE_E5,8, NOTE_C5,4, NOTE_A4,8, NOTE_G4,2,
+
+  NOTE_C5,8, NOTE_C5,4, NOTE_C5,8, REST,8, NOTE_C5,8, NOTE_D5,8, NOTE_E5,8,//13
+  REST,1, 
+  NOTE_C5,8, NOTE_C5,4, NOTE_C5,8, REST,8, NOTE_C5,8, NOTE_D5,4,
+  NOTE_E5,8, NOTE_C5,4, NOTE_A4,8, NOTE_G4,2,
+  NOTE_E5,8, NOTE_E5,8, REST,8, NOTE_E5,8, REST,8, NOTE_C5,8, NOTE_E5,4,
+  NOTE_G5,4, REST,4, NOTE_G4,4, REST,4, 
+  NOTE_C5,-4, NOTE_G4,8, REST,4, NOTE_E4,-4, // 19
+  
+  NOTE_A4,4, NOTE_B4,4, NOTE_AS4,8, NOTE_A4,4,
+  NOTE_G4,-8, NOTE_E5,-8, NOTE_G5,-8, NOTE_A5,4, NOTE_F5,8, NOTE_G5,8,
+  REST,8, NOTE_E5,4, NOTE_C5,8, NOTE_D5,8, NOTE_B4,-4,
+
+  NOTE_C5,-4, NOTE_G4,8, REST,4, NOTE_E4,-4, // repeats from 19
+  NOTE_A4,4, NOTE_B4,4, NOTE_AS4,8, NOTE_A4,4,
+  NOTE_G4,-8, NOTE_E5,-8, NOTE_G5,-8, NOTE_A5,4, NOTE_F5,8, NOTE_G5,8,
+  REST,8, NOTE_E5,4, NOTE_C5,8, NOTE_D5,8, NOTE_B4,-4,
+
+  NOTE_E5,8, NOTE_C5,4, NOTE_G4,8, REST,4, NOTE_GS4,4,//23
+  NOTE_A4,8, NOTE_F5,4, NOTE_F5,8, NOTE_A4,2,
+  NOTE_D5,-8, NOTE_A5,-8, NOTE_A5,-8, NOTE_A5,-8, NOTE_G5,-8, NOTE_F5,-8,
+  
+  NOTE_E5,8, NOTE_C5,4, NOTE_A4,8, NOTE_G4,2, //26
+  NOTE_E5,8, NOTE_C5,4, NOTE_G4,8, REST,4, NOTE_GS4,4,
+  NOTE_A4,8, NOTE_F5,4, NOTE_F5,8, NOTE_A4,2,
+  NOTE_B4,8, NOTE_F5,4, NOTE_F5,8, NOTE_F5,-8, NOTE_E5,-8, NOTE_D5,-8,
+  NOTE_C5,8, NOTE_E4,4, NOTE_E4,8, NOTE_C4,2,
+
+  NOTE_E5,8, NOTE_C5,4, NOTE_G4,8, REST,4, NOTE_GS4,4,//repeats from 23
+  NOTE_A4,8, NOTE_F5,4, NOTE_F5,8, NOTE_A4,2,
+  NOTE_D5,-8, NOTE_A5,-8, NOTE_A5,-8, NOTE_A5,-8, NOTE_G5,-8, NOTE_F5,-8,
+  
+  NOTE_E5,8, NOTE_C5,4, NOTE_A4,8, NOTE_G4,2, //26
+  NOTE_E5,8, NOTE_C5,4, NOTE_G4,8, REST,4, NOTE_GS4,4,
+  NOTE_A4,8, NOTE_F5,4, NOTE_F5,8, NOTE_A4,2,
+  NOTE_B4,8, NOTE_F5,4, NOTE_F5,8, NOTE_F5,-8, NOTE_E5,-8, NOTE_D5,-8,
+  NOTE_C5,8, NOTE_E4,4, NOTE_E4,8, NOTE_C4,2,
+  NOTE_C5,8, NOTE_C5,4, NOTE_C5,8, REST,8, NOTE_C5,8, NOTE_D5,8, NOTE_E5,8,
+  REST,1,
+
+  NOTE_C5,8, NOTE_C5,4, NOTE_C5,8, REST,8, NOTE_C5,8, NOTE_D5,4, //33
+  NOTE_E5,8, NOTE_C5,4, NOTE_A4,8, NOTE_G4,2,
+  NOTE_E5,8, NOTE_E5,8, REST,8, NOTE_E5,8, REST,8, NOTE_C5,8, NOTE_E5,4,
+  NOTE_G5,4, REST,4, NOTE_G4,4, REST,4, 
+  NOTE_E5,8, NOTE_C5,4, NOTE_G4,8, REST,4, NOTE_GS4,4,
+  NOTE_A4,8, NOTE_F5,4, NOTE_F5,8, NOTE_A4,2,
+  NOTE_D5,-8, NOTE_A5,-8, NOTE_A5,-8, NOTE_A5,-8, NOTE_G5,-8, NOTE_F5,-8,
+  
+  NOTE_E5,8, NOTE_C5,4, NOTE_A4,8, NOTE_G4,2, //40
+  NOTE_E5,8, NOTE_C5,4, NOTE_G4,8, REST,4, NOTE_GS4,4,
+  NOTE_A4,8, NOTE_F5,4, NOTE_F5,8, NOTE_A4,2,
+  NOTE_B4,8, NOTE_F5,4, NOTE_F5,8, NOTE_F5,-8, NOTE_E5,-8, NOTE_D5,-8,
+  NOTE_C5,8, NOTE_E4,4, NOTE_E4,8, NOTE_C4,2,
+  
+  //game over sound
+  NOTE_C5,-4, NOTE_G4,-4, NOTE_E4,4, //45
+  NOTE_A4,-8, NOTE_B4,-8, NOTE_A4,-8, NOTE_GS4,-8, NOTE_AS4,-8, NOTE_GS4,-8,
+  NOTE_G4,8, NOTE_D4,8, NOTE_E4,-2,  
+
+};
+
+// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
+// there are two values per note (pitch and duration), so for each note there are four bytes
+int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+
+// this calculates the duration of a whole note in ms
+int wholenote = (60000 * 4) / tempo;
+
+int divider = 0, noteDuration = 0;
+
+float ArcLength = 1.0;
+float begin = 0.0;
+
+#define RED_PIN 25
+#define GREEN_PIN 23
+#define BLUE_PIN 26
+
+const int RedChannel = 1;
+const int GreenChannel = 2;
+const int BlueChannel = 3;
+
+const int resolution = 8;
+const int freq = 250;
+
+void setup() {
+  delay(2000);
+  Serial.begin(115200);
+  Serial1.begin(115200,SERIAL_8N1, 9, 10);
+  Serial2.begin(115200, SERIAL_8N1, 16, 17);
+
+  
+  SoundMSerial.set(SoundMAngle.angle_Kp_, 2.5f);  //P gain
+  SoundMSerial.set(SoundMAngle.angle_Ki_, 2.0f);  //I gain
+  SoundMSerial.set(SoundMAngle.angle_Kd_, 0.01f); //D Gain
+
+
+  MagnetMSerial.set(MagnetMAngle.obs_angular_displacement_,0.0f); // Set current location to zero
+  MagnetMSerial.set(MagnetMAngle.trajectory_queue_mode_,(int8_t)0); // Set trajectory mode to append mode
+
+  MagnetMSerial.get(MagnetMAngle.obs_angular_displacement_, begin);
+  Serial.print("beginning angle");
+  Serial.println(begin);
+
+
+  ledcSetup(RedChannel, freq, resolution);
+  ledcSetup(GreenChannel, freq, resolution);
+  ledcSetup(BlueChannel, freq, resolution);
+
+  ledcAttachPin(RED_PIN, RedChannel);
+  ledcAttachPin(GREEN_PIN, GreenChannel);
+  ledcAttachPin(BLUE_PIN, BlueChannel);
+
+  ledcWrite(RedChannel, 125);
+  ledcWrite(GreenChannel, 125);
+  ledcWrite(BlueChannel, 125);
+
+}
+
+float temp =0.0;
+void loop() {
+  // iterate over the notes of the melody.
+  // Remember, the array is twice the number of notes (notes + durations)
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+
+    if (Serial.available()){
+
+      char c = Serial.read();
+      if (c == 'a'){
+
+        ArcLength = Serial.parseFloat();
+        Serial.println(ArcLength);
+
+      }
+    }
+
+    // calculates the duration of each note
+    divider = melody[thisNote + 1];
+    if (divider > 0) {
+      // regular note, just proceed
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      // dotted notes are represented with negative durations!!
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5; // increases the duration in half for dotted notes
+    }
+
+    // we only play the note for 90% of the duration, leaving 10% as a pause
+    SoundMSerial.set(SoundMAngle.ctrl_velocity_, (float) melody[thisNote] * scale * 6.28f);  // Set to slow speed for 1
+
+    float target = state ? - ArcLength / 2 + begin : ArcLength/2 + begin;
+    state = !state;
+    ledcWrite(RedChannel, random(0, 256));
+    ledcWrite(GreenChannel, random(0, 256));
+    ledcWrite(BlueChannel, random(0, 256));
+
+    MagnetMSerial.set(MagnetMAngle.trajectory_angular_displacement_, target);
+
+    MagnetMSerial.set(MagnetMAngle.trajectory_duration_, (float) noteDuration/1000);
+    // Wait for the specief duration before playing the next note.
+    delay(noteDuration);
+  }
+  
+}
